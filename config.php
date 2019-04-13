dcf733c196022081ff2b4007053d16850b2dac17
root <root@ip-172-31-85-6.ec2.internal>
2019-04-13 10:46:40 +0000

    this is lokesh changes

--- config.php
+++ config.php
@@ -2,4 +2,5 @@
 mysql_connect("localhost","root","");
 mysql_select_db("shop");
 ?>
-#this is the first commit form the master branch 
+#this is the first commit form the master branch
+#this is the change made by lokesh devops  
